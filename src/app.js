89a7b19f24f65c20555c248b6ab0f304f7cbf5ef
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-04-15 03:07:43 -0300

    PUT /messages/:id Data Validation and Data Lookup

--- src/app.js
+++ src/app.js
@@ -24,7 +24,6 @@ try {
     console.error(err.message);
 }
 
-//AVALIADOR: Cadastro de participante
 app.post('/participants', async (req, res) => {
 
     try {
@@ -167,7 +166,7 @@ app.delete('/messages/:id', async (req, res) => {
 
     if (!messageFind) return res.status(404).send('Message not found');
 
-    if (messageFind.from !== user) return res.status(401).send('Unauthorized deletion')
+    if (messageFind.from !== user) return res.status(401).send('Unauthorized deletion');
 
     try {
         const messages = await db.collection('messages').deleteOne({ _id: new ObjectId(id) });
@@ -178,8 +177,49 @@ app.delete('/messages/:id', async (req, res) => {
 });
 
 app.put('/messages/:id', async (req, res) => {
-    let { to, text, type } = req.body;
+    const userHeader = { User: req.header('User') };
+
+    const userSchema = joi.object({
+        to: joi.string().required(),
+        text: joi.string().required(),
+        type: joi.string().valid('message', 'private_message').required()
+    });
+
+    const userHeaderSchema = joi.object({
+        User: joi.string().required()
+    });
+
+    const validationUser = userSchema.validate(req.body, { abortEarly: false });
+
+    if (validationUser.error) {
+        const errors = validationUser.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    const validationUserHeader = userHeaderSchema.validate(userHeader, { abortEarly: false });
+
+    if (validationUserHeader.error) {
+        const errors = validationUserHeader.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+
     let user = req.header('User');
+
+    const userFind = await db.collection('participants').findOne({ name: user });
+
+    if (!userFind) {
+        return res.status(422).send('User not logged in.');
+    }
+
+    const messageFind = await db.collection('messages').findOne({ _id: new ObjectId(id) });
+
+    if (!messageFind) return res.status(404).send('Message not found');
+
+    if (messageFind.from !== user) return res.status(401).send('Unauthorized deletion');
+
+    // ++++++++++++++++++++++++++++++++++++
+    let { to, text, type } = req.body;
+    // let user = req.header('User');
     const { id } = req.params;
 
     to = sanitizeInput(to);
@@ -192,7 +232,6 @@ app.put('/messages/:id', async (req, res) => {
         to,
         text,
         type
-        // time: dayjs().format('HH:mm:ss')
     };
 
     try {
