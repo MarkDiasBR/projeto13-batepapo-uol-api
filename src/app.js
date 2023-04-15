aa58d40fe30c1ae41bf5c364eb40ac67806db413
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-04-15 04:32:00 -0300

    FIX PUT 4

--- src/app.js
+++ src/app.js
@@ -260,9 +260,9 @@ app.put('/messages/:id', async (req, res) => {
     }
 
     const messageSchema = joi.object({
-        to: joi.string(),
-        text: joi.string(),
-        type: joi.string()
+        to: joi.string().required(),
+        text: joi.string().required(),
+        type: joi.string().required()
     });
 
     const validation = messageSchema.validate(req.body, { abortEarly: false });
